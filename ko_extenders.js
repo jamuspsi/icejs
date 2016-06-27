8f4518e36434d2dd720a4e7d8ca39b928b8215f7
Brian Arsuaga <brian.arsuaga@gmail.com>
2016-06-26 23:00:58 -0500

    adding

--- ko_extenders.js
+++ ko_extenders.js
@@ -69,7 +69,23 @@ ko.extenders.trim = function clampObservable(obs, opts) {
     };
     obs.trimmed = trimmed;
     return obs;
-}
+};
+
+ko.extenders.throttle = function(obs, opts) {
+    var throttle = ko.observable(false);
+    var throttled = ko.computed(function() {
+        throttle();
+        return obs.peek();
+    });
+    var trigger = _.throttle(function() {
+        throttle(!throttle());
+    }, 1000);
+
+    obs.subscribeChanged(trigger);
+
+    obs.throttled = throttled;
+    return obs;
+};
 
 
 ko.extenders.dirty_tracker = function(obs, opts) {
@@ -183,3 +199,6 @@ ko.computed.fn.toString = function() {
     return "computed: " + ko.toJSON(this(), null, 2);
 };
 
+ko.observable.fn.inc = function(v) {
+    this(this() + v);
+}
